97386ddd491a923439d143b9b846e7ac8aec6f2c
ArlandMv <arlandmichelenav@gmail.com>
2022-02-26 11:55:49 -0300

    build: add client.js which integrates sanity backend to project

--- client/src/container/About/About.jsx
+++ client/src/container/About/About.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
-import './About.scss';
+
 import { images } from '../../constants';
+import './About.scss';
+import { urlFor, client } from '../../client';
 const abouts = [
 	{
 		title: 'UX/UI',
@@ -25,20 +27,23 @@ const abouts = [
 	},
 ];
 
-/*  Es bien sabido que
-  	<span>buen diseño</span>
-		puede ser
-		<span>una gran ventaja</span>
-		para su negocio o empresa 
- */
-
 const About = () => {
+	const [abouts, setAbouts] = useState([]);
+
+	useEffect(() => {
+		const query = '*[_type == "abouts"]';
+		client
+			.fetch(query)
+			.then((data) => setAbouts(data))
+			.then(console.log('useEffect DONE'));
+	}, []);
+
 	return (
 		<>
 			<h2 className="head-text">
 				una buena <span>presencia online</span>
 				<br />
-				implica un<span> negocio exitoso</span>
+				implica un <span> negocio exitoso</span>
 			</h2>
 
 			<div className="app__profiles">
@@ -49,7 +54,7 @@ const About = () => {
 						transition={{ duration: 0.5, type: 'tween' }}
 						className="app__profile-item"
 						key={about.title + index}>
-						<img src={about.imgUrl} alt={about.title} />
+						<img src={urlFor(about.imgUrl)} alt={about.title} />
 						<h2 className="bold-text" style={{ marginTop: 20 }}>
 							{about.title}
 						</h2>
