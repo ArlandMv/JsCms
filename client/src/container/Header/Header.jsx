674693d83c26fd11da38db13eb337478c0c81957
ArlandMv <arlandmichelenav@gmail.com>
2022-02-26 14:43:15 -0300

    feat: add feature CMS with sanity.io and HighOrderComponent for scrolling

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-
+import { AppWrap } from '../../wrapper';
 import { images } from '../../constants';
 import './Header.scss';
 
@@ -64,4 +64,5 @@ const Header = () => (
 	</div>
 );
 
-export default Header;
+/* export default Header; */
+export default AppWrap(Header, 'home');
